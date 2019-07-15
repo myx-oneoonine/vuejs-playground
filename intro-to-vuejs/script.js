32fc91e86895e2d125e5a249cbaf94f12a88c623
Phanuphat Cholatda <phanuphat.cho@gmail.com>
2019-07-15 17:19:24 +0700

    succeed lab 2

--- intro-to-vuejs/script.js
+++ intro-to-vuejs/script.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "blablabla"
+        description: "blablabla",
+        image: "./vmSocks-green.png",
+        link: "http://google.co.th"
     }
 })
